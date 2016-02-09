73f920f848b2dc3b2ef08835fd34701039fb84d7
gongchen322 <gong27@purdue.edu>
2016-02-08 18:15:34 -0800

    Add Angular UI Router

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var jquery = require('jquery');
-var angular = require('./js/lib/angular');
+//var jquery = require('jquery');
+//var angular = require('./js/lib/angular');
 
 
-var myApp = angular.module('myApp', []);
-myApp.controller('mainCtrl', require('./js/controllers/mainCtrl'));
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'js/view/partial-home.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('shop', {
+          	url: '/shop',
+            templateUrl: 'js/view/shop.html'
+        });
+        
+});
+
+//myApp.controller('mainCtrl', require('./js/controllers/mainCtrl'));
