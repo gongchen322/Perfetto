780349c54e9aca3b2ba211fa93eb5a3ad2ab54bc
gongchen322 <gong27@purdue.edu>
2016-02-11 14:20:56 -0800

    CSS Update
    Added image hover effect

--- js/controllers/menController.js
+++ js/controllers/menController.js
@@ -2,11 +2,11 @@
     
     var menController = function ($scope) {
         $scope.items = [
-        {id:'1',source:'assets/pictures/clothing/men/1s.jpg', name:'Charles Black', price:'1150 USD'},
-        {id:'2',source:'assets/pictures/clothing/men/2s.jpg', name:'Minuto Balsam Green', price:'1250 USD'},
-        {id:'3',source:'assets/pictures/clothing/men/3s.jpg', name:'Charles Charcoal Gray', price:'1150 USD'},
-        {id:'4',source:'assets/pictures/clothing/men/4s.jpg', name:'Who Black Black', price:'340  USD'},
-        {id:'5',source:'assets/pictures/clothing/men/5s.jpg', name:'Apolo double charcoal grey', price:'750 USD'},
+        {id:'1',source:'assets/pictures/clothing/men/1s.jpg',name:'Double wool cashmere coat', color:'Charles Black', price:'1150 USD'},
+        {id:'2',source:'assets/pictures/clothing/men/2s.jpg',name:'Belted trenchcoat', color:'Minuto Balsam Green', price:'1250 USD'},
+        {id:'3',source:'assets/pictures/clothing/men/3s.jpg',name:'Charles Charcoal Gray', color:'Charles Charcoal Gray', price:'1150 USD'},
+        {id:'4',source:'assets/pictures/clothing/men/4s.jpg',name:'Shiny bomber jacket', color:'Who Black Black', price:'340  USD'},
+        {id:'5',source:'assets/pictures/clothing/men/5s.jpg',name:'Apolo double charcoal grey', color:'Apolo double charcoal grey', price:'750 USD'},
         ];
     };
     
