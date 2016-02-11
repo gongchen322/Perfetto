2561cc362a40c1fcf6eb85cbfa8534a267d0e6ef
gongchen322 <gong27@purdue.edu>
2016-02-11 00:38:26 -0800

    Update CSS

--- js/controllers/menController.js
+++ js/controllers/menController.js
@@ -1,12 +1,12 @@
 (function() {
     
     var menController = function ($scope) {
-        $scope.imgs = [
-        {id:'1',source:'assets/pictures/clothing/men/1s.jpg'},
-        {id:'2',source:'assets/pictures/clothing/men/2s.jpg'},
-        {id:'3',source:'assets/pictures/clothing/men/3s.jpg'},
-        {id:'4',source:'assets/pictures/clothing/men/4s.jpg'},
-        {id:'5',source:'assets/pictures/clothing/men/5s.jpg'},
+        $scope.items = [
+        {id:'1',source:'assets/pictures/clothing/men/1s.jpg', name:'Charles Black', price:'1150 USD'},
+        {id:'2',source:'assets/pictures/clothing/men/2s.jpg', name:'Minuto Balsam Green', price:'1250 USD'},
+        {id:'3',source:'assets/pictures/clothing/men/3s.jpg', name:'Charles Charcoal Gray', price:'1150 USD'},
+        {id:'4',source:'assets/pictures/clothing/men/4s.jpg', name:'Who Black Black', price:'340  USD'},
+        {id:'5',source:'assets/pictures/clothing/men/5s.jpg', name:'Apolo double charcoal grey', price:'750 USD'},
         ];
     };
     
