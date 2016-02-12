67721af5536a62fb3283d801dee5701a7bd58a3a
gongchen322 <gong27@purdue.edu>
2016-02-11 23:45:40 -0800

    Updated Popup element

--- js/controllers/menController.js
+++ js/controllers/menController.js
@@ -1,14 +1,21 @@
 (function() {
-    
+    $(".popup_item").on('blur',function(){
+    	$(this).fadeOut(300);
+	});
+
+
+
     var menController = function ($scope,$uibModal, $log) {
         $scope.items = [
-        {id:'1',source:'assets/pictures/clothing/men/1s.jpg',name:'Double wool cashmere coat', color:'Charles Black', price:'1150 USD'},
-        {id:'2',source:'assets/pictures/clothing/men/2s.jpg',name:'Belted trenchcoat', color:'Minuto Balsam Green', price:'1250 USD'},
-        {id:'3',source:'assets/pictures/clothing/men/3s.jpg',name:'Charles Charcoal Gray', color:'Charles Charcoal Gray', price:'1150 USD'},
-        {id:'4',source:'assets/pictures/clothing/men/4s.jpg',name:'Shiny bomber jacket', color:'Who Black Black', price:'340  USD'},
-        {id:'5',source:'assets/pictures/clothing/men/5s.jpg',name:'Apolo double charcoal grey', color:'Apolo double charcoal grey', price:'750 USD'},
+        {id:'1',source:'assets/pictures/clothing/men/1s.jpg',source_large:'assets/pictures/clothing/men/1.jpg',name:'Double wool cashmere coat', color:'Charles Black', price:'1150 USD'},
+        {id:'2',source:'assets/pictures/clothing/men/2s.jpg',source_large:'assets/pictures/clothing/men/2.jpg',name:'Belted trenchcoat', color:'Minuto Balsam Green', price:'1250 USD'},
+        {id:'3',source:'assets/pictures/clothing/men/3s.jpg',source_large:'assets/pictures/clothing/men/3.jpg',name:'Charles Charcoal Gray', color:'Charles Charcoal Gray', price:'1150 USD'},
+        {id:'4',source:'assets/pictures/clothing/men/4s.jpg',source_large:'assets/pictures/clothing/men/4.jpg',name:'Shiny bomber jacket', color:'Who Black Black', price:'340  USD'},
+        {id:'5',source:'assets/pictures/clothing/men/5s.jpg',source_large:'assets/pictures/clothing/men/5.jpg',name:'Apolo double charcoal grey', color:'Apolo double charcoal grey', price:'750 USD'},
         ];
+
         $scope.id=1;
+        
         $scope.animationsEnabled = true;
 
         $scope.open = function (id) {
@@ -37,13 +44,17 @@
     		$scope.animationsEnabled = !$scope.animationsEnabled;
   		};
 
+  		$scope.setSize = function (size) {
+    			$scope.size=size;
+  		};
+
     };
 
     var ModalInstanceCtrl = function ($scope, $uibModalInstance, items, id) {
         $scope.items = items;
         $scope.id = id;
   		$scope.item=items[id];
-
+  		$scope.size='Small';
   		$scope.ok = function () {
 	    $uibModalInstance.close($scope.selected.item);
 	  };
@@ -51,7 +62,9 @@
 	  $scope.cancel = function () {
 	    $uibModalInstance.dismiss('cancel');
 	  };
-
+	  $scope.setSize = function (size) {
+    			$scope.size=size;
+  		};
     };  
 
   	menController.$inject = ['$scope', '$uibModal','$log'];
